f5521bde7eadabb7fcdee439b6ddf60dc653e7d7
Filip Soukup <ffilipsoukup@gmail.com>
2020-11-19 15:00:32 +0100

    Config service

---
+++ src/services/settings.ts
@@ -0,0 +1,25 @@
+import * as vscode from 'vscode';
+
+const CONFIG_KEY: string = 'al-id-range-manager';
+
+export class Settings {
+    private apiBaseUrl?: string;
+    private apiUsername?: string;
+    private apiPassword?: string;
+
+    public get ApiBaseUrl() { return this.apiBaseUrl; }
+    public get ApiUsername() { return this.apiUsername; }
+    public get ApiPassword() { return this.apiPassword; }
+
+    constructor() {
+        this.parseConfig();
+    }
+
+    private parseConfig() {
+        let config = vscode.workspace.getConfiguration(CONFIG_KEY);
+
+        this.apiBaseUrl = config.get('baseUrl');
+        this.apiUsername = config.get('username');
+        this.apiPassword = config.get('password');
+    }
+}
